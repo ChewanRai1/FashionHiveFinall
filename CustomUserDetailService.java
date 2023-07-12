572ae0c8804469d0c1ab84507b8d7765937fa84b
bhumikaasingh <bhumika8singh@gmail.com>
2023-07-12 18:18:10 +0545

    initial

--- CustomUserDetailService.java
+++ CustomUserDetailService.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
+//customer user
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailService implements UserDetailsService {
